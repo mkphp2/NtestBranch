095fc42b9a3e5a54a331b7d7e178f0468eeeaf62
MITHU-PC\abc <mkphp2@gmail.com>
2018-08-24 16:05:41 +0530

    New Rep

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,9 @@
+<?php
+   namespace HelloWorld;
+   class SayHello {
+      public static function world() {
+         return 'Hello Tejas, your Extension is successfully run! Well Done.
+		 This is yii2 extension.';
+      }
+   }
+?>
